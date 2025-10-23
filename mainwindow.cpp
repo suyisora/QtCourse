c79e72e85dd1e41eace86283a9512ca5806ebc83
zyc <3079452260@qq.com>
2025-10-23 20:20:00 +0800

    the_1

---
+++ mainwindow.cpp
@@ -0,0 +1,199 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <math.h>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    digitBTNs = {{Qt::Key_0, ui->btnNum0},
+               {Qt::Key_1, ui->btnNum1},
+               {Qt::Key_2, ui->btnNum2},
+               {Qt::Key_3, ui->btnNum3},
+               {Qt::Key_4, ui->btnNum4},
+               {Qt::Key_5, ui->btnNum5},
+               {Qt::Key_6, ui->btnNum6},
+               {Qt::Key_7, ui->btnNum7},
+               {Qt::Key_8, ui->btnNum8},
+               {Qt::Key_9, ui->btnNum9},
+            };
+
+    foreach(auto btn,digitBTNs){
+        connect(btn,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    }
+
+    // connect(ui->btnNum0,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum1,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum2,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum3,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum4,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum5,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum6,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum7,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum8,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    // connect(ui->btnNum9,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+
+    connect(ui->btnMultable,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btndivision,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnPlus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnminus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+
+    connect(ui->btnPecentage,SIGNAL(clicked()),this,SLOT(btnUnaryOperatorClicked()));
+    connect(ui->btnSquare,SIGNAL(clicked()),this,SLOT(btnUnaryOperatorClicked()));
+    connect(ui->btnSquareroot,SIGNAL(clicked()),this,SLOT(btnUnaryOperatorClicked()));
+    connect(ui->btnfraction,SIGNAL(clicked()),this,SLOT(btnUnaryOperatorClicked()));
+    connect(ui->btnMaxOrMin,SIGNAL(clicked()),this,SLOT(btnUnaryOperatorClicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+QString MainWindow::caculation(bool *ok)
+{
+    double result = 0;
+    if(operands.size() == 2 && opcodes.size() > 0){
+    //取操作数
+    double operand1 = operands.front().toDouble();
+    operands.pop_front();
+    double operand2 = operands.front().toDouble();
+    operands.pop_front();
+
+
+    //取操作符
+    QString op = opcodes.front();
+    opcodes.pop_front();
+
+    if(op == "+"){
+        result = operand1 + operand2;
+    }else if(op == "-"){
+        result = operand1 - operand2;
+    }else if(op == "×"){
+        result = operand1 * operand2;
+    }else if(op == "÷"){
+        result = operand1 / operand2;
+    }
+
+
+    operands.push_back(QString::number(result));
+
+        ui->statusbar->showMessage(QString("caculation is in process:operand is %1,opcode is %2").arg(operands.size()).arg(opcodes.size()));
+    }else{
+        ui->statusbar->showMessage(QString("operand is %1,opcode is %2").arg(operands.size()).arg(opcodes.size()));
+    }
+
+    return QString::number(result);
+}
+
+void MainWindow::btnNumClicked()
+{
+    QString digit = qobject_cast<QPushButton *>(sender())->text();
+
+    if( digit == "0" && operand == "0")
+        digit = "";
+
+    if( operand == "0" && digit != "0")
+        operand = "";
+
+    operand += digit;
+    ui->display->setText(operand);
+
+}
+
+
+
+void MainWindow::on_btnPoint_clicked()
+{
+    if(!operand.contains("."))
+        operand+=qobject_cast<QPushButton *>(sender())->text();
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_btnDelete_clicked()
+{
+    operand = operand.left(operand.length()-1);
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_C_clicked()
+{
+    operand.clear();
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::btnBinaryOperatorClicked()
+{
+    QString opcode = qobject_cast<QPushButton *>(sender())->text();
+
+    if(operand != ""){
+        operands.push_back(operand);
+        operand = "";
+
+        opcodes.push_back(opcode);
+
+
+        QString result = caculation();
+        ui->display->setText(result);
+    }
+}
+
+void MainWindow::btnUnaryOperatorClicked()
+{
+    if(operand != ""){
+        double result = operand.toDouble();
+        operand = "";
+
+        QString op = qobject_cast<QPushButton*>(sender())->text();
+
+        if(op == "%"){
+            result /= 100.0;
+        }else if(op == "1/x"){
+            result = 1/result;
+        }else if(op == "x^2"){
+            result = result * result;
+        }else if(op == "√"){
+            result = sqrt(result);
+        }else if(op == "+/-"){
+            if(result > 0) result = -result;
+            else result = +result;
+        }
+
+        ui->display->setText(QString::number(result));
+    }
+
+}
+
+void MainWindow::on_btnequal_clicked()
+{
+    if(operand != ""){
+        operands.push_back(operand);
+        operand = "";
+    }
+
+    QString result = caculation();
+    ui->display->setText(result);
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+
+    foreach (auto btnKey, digitBTNs.keys()) {
+        if(event->key()== btnKey){
+            digitBTNs[btnKey]->animateClick();
+        }
+    }
+
+    // if(event -> key() == Qt::Key_0)
+    //     ui->btnNum0->animateClick();
+    // else if (event -> key() == Qt::Key_1)
+    //     ui->btnNum1->animateClick();
+
+}
+
+
---
+++ mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QStack>
+#include <QKeyEvent>
+#include <QMap>
+#include <QPushButton>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    QString operand;
+    QStack<QString> operands;
+
+    QString opcode;
+    QStack<QString> opcodes;
+
+    QMap<int, QPushButton*> digitBTNs;
+
+    QString caculation(bool *ok=NULL);
+
+private slots:
+    void btnNumClicked();
+    void btnBinaryOperatorClicked();
+    void btnUnaryOperatorClicked();
+
+    void on_btnPoint_clicked();
+
+    void on_btnDelete_clicked();
+
+    void on_C_clicked();
+
+    void on_btnequal_clicked();
+
+    virtual void keyPressEvent(QKeyEvent *event);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
